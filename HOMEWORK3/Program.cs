a8af2119ec9c852576046aa8536268ad2e1970b1
Дима <kitaykin9808@gmail.com>
2021-01-23 17:50:40 +0300

    добавил 3 задание

---
+++ HOMEWORK3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HOMEWORK3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вывести на экран последовательность чисел y, " +
+                             "\nявляющихся результатом выполнения операции y = A sin x, где A=0,5," +
+                             "\nа переменная x изменяется от 1 до 10 с шагом 1.");
+            Console.WriteLine();
+
+            double A = 0.5;
+            double x = 1;
+
+            while(x<=10)
+            {
+                Console.WriteLine($"{A} * sin {x} = {A*Math.Sin(x)}");
+                x++;
+            }
+        }
+    }
+}
